d7803dcc6e9b208eea984b97f679f1106ac6e5af
aocha <ao.hk215@gmail.com>
2023-04-02 09:17:47 +0900

    Homebrewのインストールを追加

---
+++ bin/install_homebrew.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "[info] Start install Homebrew..."
+if ! command -v brew &> /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "[info] Finish install Homebrew."
+else
+  echo "[info] Homebrew is already installed."
+fi
